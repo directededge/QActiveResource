7d736785cb510e91c53b802d645c786077b0280c
Scott Wheeler <scott@directededge.com>
2010-05-06 13:49:30 +0200

    Get find working properly (without options, for now)

--- Ruby/QAR.cpp
+++ Ruby/QAR.cpp
@@ -1,4 +1,5 @@
 #include <ruby.h>
+
 #include "ActiveResource.h"
 
 extern "C"
@@ -24,10 +25,68 @@ extern "C"
         return Data_Wrap_Struct(klass, resource_mark, resource_free, resource);
     }
 
+    static VALUE resource_initialize(VALUE self, VALUE base, VALUE resource)
+    {
+        ActiveResource::Resource *r = 0;
+        Data_Get_Struct(self, class ActiveResource::Resource, r);
+        r->setBase(QString::fromUtf8(rb_string_value_ptr(&base)));
+        r->setResource(QString::fromUtf8(rb_string_value_ptr(&resource)));
+    }
+
+    static VALUE to_value(const QVariant &v)
+    {
+        switch(v.type())
+        {
+        case QVariant::Hash:
+        {
+            VALUE value = rb_hash_new();
+
+            QHash<QString, QVariant> hash = v.toHash();
+
+            for(QHash<QString, QVariant>::ConstIterator it = hash.begin(); it != hash.end(); ++it)
+            {
+                rb_hash_aset(value, rb_str_new2(it.key().toUtf8()), to_value(it.value()));
+            }
+
+            return value;
+        }
+        case QVariant::List:
+        {
+            VALUE value = rb_ary_new();
+
+            foreach(QVariant element, v.toList())
+            {
+                rb_ary_push(value, to_value(element));
+            }
+
+            return value;
+        }
+        case QVariant::Bool:
+            return v.toBool() ? Qtrue : Qfalse;
+        case QVariant::Int:
+            return rb_int_new(v.toInt());
+        case QVariant::Double:
+            return rb_float_new(v.toDouble());
+        default:
+            return rb_str_new2(v.toString().toUtf8());
+        }
+    }
+
     static VALUE resource_find(VALUE self)
     {
-        printf("find!\n");
-        return 0;
+        ActiveResource::Resource *resource = 0;
+        Data_Get_Struct(self, class ActiveResource::Resource, resource);
+
+        ActiveResource::RecordList records = resource->find();
+
+        VALUE array = rb_ary_new2(records.length());
+
+        for(int i = 0; i < records.length(); i++)
+        {
+            rb_ary_store(array, i, to_value(records[i]));
+        }
+
+        return array;
     }
 
     void Init_QAR(void)
@@ -35,6 +94,7 @@ extern "C"
         rb_mQAR = rb_define_module("QAR");
         rb_cQARResource = rb_define_class_under(rb_mQAR, "Resource", rb_cObject);
         rb_define_alloc_func(rb_cQARResource, resource_allocate);
+        rb_define_method(rb_cQARResource, "initialize", (ARGS) resource_initialize, 2);
         rb_define_method(rb_cQARResource, "find", (ARGS) resource_find, 0);
     }
 }
