54d1ae73e367296f779538d0d0babad84d5847b2
Scott Wheeler <scott@directededge.com>
2010-05-10 18:42:37 +0200

    Build on Linux

--- Ruby/extconf.rb
+++ Ruby/extconf.rb
@@ -4,8 +4,18 @@ dir_config("QAR")
 $CFLAGS << " -I/usr/local/include -I.."
 $LIBS << " -lqactiveresource"
 
-qt_dir = File.readlink(`which qmake`.chomp).sub('/bin/qmake', '')
+qmake_path = `which qmake`.chomp
 
-pkg_config("#{qt_dir}/lib/pkgconfig/QtCore.pc")
+if qmake_path.empty?
+  warn "********************************************************************************"
+  warn 'Cannot find qmake (should be installed with Qt development packages)'
+  warn "********************************************************************************"
+  exit
+end
+
+qmake_path = File.readlink(qmake_path) if File.symlink?(qmake_path)
+qt_path = qmake_path.sub('/bin/qmake', '')
+
+pkg_config("#{qt_path}/lib/pkgconfig/QtCore.pc")
 
 create_makefile("QAR")
