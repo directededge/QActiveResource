9c393052d225a423b1c89bf9ebcb836eaf5c7589
Scott Wheeler <scott@directededge.com>
2010-05-10 15:09:21 +0200

    Use our Response class in the created exceptions.

--- Ruby/QAR.cpp
+++ Ruby/QAR.cpp
@@ -25,6 +25,25 @@ static VALUE rb_mActiveResource;
 static VALUE rb_cActiveResourceBase;
 static VALUE rb_cQARParamList;
 static VALUE rb_cQARResource;
+static VALUE rb_cQARResponse;
+
+/*
+ * Exceptions
+ */
+
+static VALUE rb_eActiveResourceConnectionError;
+static VALUE rb_eActiveResourceTimeoutError;
+static VALUE rb_eActiveResourceSSLError;
+static VALUE rb_eActiveResourceRedirection;
+static VALUE rb_eActiveResourceClientError;
+static VALUE rb_eActiveResourceBadRequest;
+static VALUE rb_eActiveResourceUnauthorizedAccess;
+static VALUE rb_eActiveResourceForbiddenAccess;
+static VALUE rb_eActiveResourceResourceNotFound;
+static VALUE rb_eActiveResourceMethodNotAllowed;
+static VALUE rb_eActiveResourceResourceConflict;
+static VALUE rb_eActiveResourceResourceGone;
+static VALUE rb_eActiveResourceServerError;
 
 /*
  * Symbols
@@ -33,11 +52,14 @@ static VALUE rb_cQARResource;
 static const ID _all = rb_intern("all");
 static const ID _allocate = rb_intern("allocate");
 static const ID _at = rb_intern("at");
+static const ID _code = rb_intern("code");
+static const ID _body = rb_intern("body");
 static const ID _collection_name = rb_intern("collection_name");
 static const ID _element_name = rb_intern("element_name");
 static const ID _extend = rb_intern("extend");
 static const ID _first = rb_intern("first");
 static const ID _from = rb_intern("from");
+static const ID _headers = rb_intern("headers");
 static const ID _new = rb_intern("new");
 static const ID _one = rb_intern("one");
 static const ID _params = rb_intern("params");
@@ -51,13 +73,27 @@ static const ID _last = rb_intern("last");
  */
 
 static const ID __attributes = rb_intern("@attributes");
+static const ID __code = rb_intern("@code");
+static const ID __body = rb_intern("@body");
+static const ID __headers = rb_intern("@headers");
 static const ID __prefix_options = rb_intern("@prefix_options");
-static const ID __qar_resource = rb_intern("@@qar_resource");
+static const ID __response = rb_intern("@response");
+
+/*
+ * Class variable symbols
+ */
+
+static const ID ___qar_resource = rb_intern("@@qar_resource");
 
 static QString to_s(VALUE value)
 {
     VALUE s = rb_funcall(value, _to_s, 0);
     return QString::fromUtf8(StringValuePtr(s));
+}
+
+static VALUE to_value(const QString &s)
+{
+    return rb_str_new2(s.toUtf8());
 }
 
 static VALUE to_value(const QVariant &v, VALUE base, bool isChild = false)
@@ -85,7 +121,7 @@ static VALUE to_value(const QVariant &v, VALUE base, bool isChild = false)
 
         for(QActiveResource::Record::ConstIterator it = record.begin(); it != record.end(); ++it)
         {
-            VALUE key = rb_str_new2(it.key().toUtf8());
+            VALUE key = to_value(it.key());
             VALUE value = to_value(it.value(), base, true);
             rb_hash_aset(attributes, key, value);
         }
@@ -119,8 +155,20 @@ static VALUE to_value(const QVariant &v, VALUE base, bool isChild = false)
         return rb_funcall(rb_cTime, _at, 1, rb_int_new(v.toDateTime().toTime_t()));
     }
     default:
-        return rb_str_new2(v.toString().toUtf8());
+        return to_value(v.toString());
     }
+}
+
+static VALUE to_value(const QHash<QString, QString> &hash)
+{
+    VALUE values = rb_hash_new();
+
+    for(QHash<QString, QString>::ConstIterator it = hash.begin(); it != hash.end(); ++it)
+    {
+        rb_hash_aset(values, to_value(it.key()), to_value(it.value()));
+    }
+
+    return values;
 }
 
 /*
@@ -163,12 +211,23 @@ static int params_hash_iterator(VALUE key, VALUE value, VALUE params)
 }
 
 /*
+ * Response
+ */
+
+static VALUE response_initialize(VALUE self, VALUE code, VALUE headers, VALUE body)
+{
+    rb_ivar_set(self, __code, code);
+    rb_ivar_set(self, __headers, headers);
+    rb_ivar_set(self, __body, body);
+}
+
+/*
  * QAR
  */
 
 static QActiveResource::Resource *get_resource(VALUE self)
 {
-    VALUE member = rb_cvar_get(self, __qar_resource);
+    VALUE member = rb_cvar_get(self, ___qar_resource);
     QActiveResource::Resource *resource = 0;
     Data_Get_Struct(member, QActiveResource::Resource, resource);
     return resource;
@@ -242,13 +301,20 @@ static VALUE qar_find(int argc, VALUE *argv, VALUE self)
     }
     catch(QActiveResource::Exception ex)
     {
+        VALUE code = rb_int_new(ex.response().code());
+        VALUE response = rb_funcall(rb_cQARResponse, _new, 3,
+                                    code,
+                                    to_value(ex.response().headers()),
+                                    rb_str_new2(ex.response().data()));
 
         #define AR_TEST_EXCEPTION(name)                                 \
             if(ex.type() == QActiveResource::Exception::name)           \
             {                                                           \
-                QString s("ActiveResource::" #name ".new(\"%1\")");     \
-                VALUE e = rb_eval_string(s.arg(ex.message()).toUtf8()); \
-                rb_funcall(rb_mKernel, _raise, 1, e);                   \
+                VALUE e = rb_funcall(                                   \
+                    rb_eActiveResource##name, _allocate, 0);            \
+                rb_ivar_set(e, __code, code);                           \
+                rb_ivar_set(e, __response, response);                   \
+                rb_exc_raise(e);                                        \
             }
 
         AR_TEST_EXCEPTION(ConnectionError);
@@ -279,7 +345,7 @@ static VALUE set_follow_redirects(VALUE self, VALUE follow)
 static VALUE qar_extended(VALUE self, VALUE base)
 {
     VALUE resource = rb_funcall(rb_cQARResource, _new, 0);
-    rb_cvar_set(base, __qar_resource, resource, false);
+    rb_cvar_set(base, ___qar_resource, resource, false);
     return Qnil;
 }
 
@@ -287,12 +353,41 @@ extern "C"
 {
     void Init_QAR(void)
     {
+        #define DEFINE_CLASS(module, name) \
+            rb_c##module##name = rb_define_class_under(rb_m##module, #name, rb_cObject)
+
         rb_mActiveResource = rb_define_module("ActiveResource");
-        rb_cActiveResourceBase = rb_define_class_under(rb_mActiveResource, "Base", rb_cObject);
+
+        DEFINE_CLASS(ActiveResource, Base);
+
+        #define AR_DEFINE_EXCEPTION(name, base) \
+            rb_eActiveResource##name = rb_define_class_under(rb_mActiveResource, #name, rb_e##base)
+
+        AR_DEFINE_EXCEPTION(ConnectionError, StandardError);
+        AR_DEFINE_EXCEPTION(TimeoutError, ActiveResourceConnectionError);
+        AR_DEFINE_EXCEPTION(SSLError, ActiveResourceConnectionError);
+        AR_DEFINE_EXCEPTION(Redirection, ActiveResourceConnectionError);
+        AR_DEFINE_EXCEPTION(ClientError, ActiveResourceConnectionError);
+        AR_DEFINE_EXCEPTION(BadRequest, ActiveResourceClientError);
+        AR_DEFINE_EXCEPTION(UnauthorizedAccess, ActiveResourceClientError);
+        AR_DEFINE_EXCEPTION(ForbiddenAccess, ActiveResourceClientError);
+        AR_DEFINE_EXCEPTION(ResourceNotFound, ActiveResourceClientError);
+        AR_DEFINE_EXCEPTION(MethodNotAllowed, ActiveResourceClientError);
+        AR_DEFINE_EXCEPTION(ResourceConflict, ActiveResourceClientError);
+        AR_DEFINE_EXCEPTION(ResourceGone, ActiveResourceClientError);
+        AR_DEFINE_EXCEPTION(ServerError, ActiveResourceConnectionError);
 
         rb_mQAR = rb_define_module("QAR");
 
-        rb_cQARParamList = rb_define_class_under(rb_mQAR, "ParamList", rb_cObject);
+        DEFINE_CLASS(QAR, Response);
+        rb_define_method(rb_cQARResponse, "initialize", (ARGS) response_initialize, 3);
+        rb_attr(rb_cQARResponse, _code, 1, 0, Qfalse);
+        rb_attr(rb_cQARResponse, _headers, 1, 0, Qfalse);
+        rb_attr(rb_cQARResponse, _body, 1, 0, Qfalse);
+
+        DEFINE_CLASS(QAR, ParamList);
+        rb_define_alloc_func(rb_cQARResponse, resource_allocate);
+
         rb_define_alloc_func(rb_cQARParamList, param_list_allocate);
 
         rb_cQARResource = rb_define_class_under(rb_mQAR, "Resource", rb_cObject);
