4548cf5ef201afec2cb7d3b033e0372fe0f8c745
Scott Wheeler <scott@directededge.com>
2011-05-24 15:18:44 +0200

    Build on systems that use lib64 dirs (i.e. SUSE)

--- Ruby/extconf.rb
+++ Ruby/extconf.rb
@@ -21,6 +21,17 @@ else
   ENV['PKG_CONFIG_PATH'] = "#{qt_path}/lib/pkgconfig"
 end
 
-pkg_config("#{qt_path}/lib/pkgconfig/QtNetwork.pc")
+pc = "#{qt_path}/lib64/pkgconfig/QtNetwork.pc"
+
+if !File.exists? pc
+  pc = "#{qt_path}/lib/pkgconfig/QtNetwork.pc"
+end
+
+if !File.exists? pc
+  warn "Could not find QtNetwork.pc"
+  exit 1
+end
+
+pkg_config(pc)
 
 create_makefile("QAR")
